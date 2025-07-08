104442fbe7da108fdefa3b41413e7f5c8530b00f
GuyForShort <GiteousMaximus@icloud.com>
2025-07-08 20:08:25 +0100

    Groups added

--- main.py
+++ main.py
@@ -1,33 +1,63 @@
+# powered by pygame tm
 import pygame
+#player.py holds player class and functions
+
 from player import *
+
+
+# constants holds all the  ... constants
 from constants import *
+#still not 100% sure why we run the program in a function... 
 def main():
+
+# set up pygame defaults
     pygame.init()
 
+# let user know things are happening, and resolution set
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+# set up groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    player.containers = (updatable, drawable)
+#pygame magic to make a playing window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# deffo did this wrong, but just in case there is a need to kill the game, update this variable with a key press
     game = "on"
+
+# tick tick etc
     clock = pygame.time.Clock() 
+
+# to get the frame rate relate to the other defined speeds in game, stopping things going to fast 
     dt = 0
+    
+
+# Create the player (triangle but really a circle) ship
     Player = player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2),PLAYER_RADIUS)
 
+
+# litteral gameplay loop
     while game == "on":
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+# paint it all black
         screen.fill(000)
-        Player.update(dt)
-        Player.draw(screen)
+    #do the space ship and rock things like flying n stuff
+        updatable.update(dt)
+    #and show that on the screen
+        for thing in drawable:
+            thing.draw(screen)
+    # frame happens
         pygame.display.flip()
+    # frame rate limit and adjustment for ms for ratios in other places
         dt = clock.tick(60) /1000
         
 
 
 
-
+# only run if actually run as main.py
 if __name__ == "__main__":
     main()
 
